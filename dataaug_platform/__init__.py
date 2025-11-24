062a35f45a07f7e89a6062725613ead4d4c16e1b
Mrinal Jain <2mrinaljain@gmail.com>
2025-11-23 23:48:00 -0500

    make mimicgen deps optional

--- dataaug_platform/__init__.py
+++ dataaug_platform/__init__.py
@@ -7,7 +7,6 @@ from .augmentations.base_augmentation import (
     local_aug,
     global_aug,
 )
-from .augmentations.mimicgen import MimicGenAugmentation
 from .pipeline import Pipeline
 from .ingestion import load_hdf5_group, hdf5_to_rdd, read_hdf5_metadata, write_trajectories_to_hdf5
 
@@ -20,7 +19,13 @@ __all__ = [
     "hdf5_to_rdd",
     "read_hdf5_metadata",
     "write_trajectories_to_hdf5",
-    "MimicGenAugmentation",
 ]
+
+# Optional imports - only available if mimicgen dependencies are installed
+try:
+    from .augmentations.mimicgen import MimicGenAugmentation
+    __all__.append("MimicGenAugmentation")
+except ImportError:
+    pass
 
 __version__ = "0.1.0"
